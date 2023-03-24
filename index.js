c9e8190898e2297c94998a99d11a19af2fdcf896
Sarah Jones <sarahrjones8@gmail.com>
2023-03-23 22:48:08 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function (res) {
+      return res.json();
+    })  
+    .then(function (data) {
+      return renderBooks(data);
+    })
 }
 
 function renderBooks(books) {
@@ -15,3 +20,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
